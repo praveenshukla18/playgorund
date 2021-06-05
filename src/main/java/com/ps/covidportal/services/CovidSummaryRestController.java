2809033517caa9ca26f3cfc0e6921c568b76bb89
praveen <praveenshkl18@outlook.com>
2021-06-05 23:39:19 +0530

    Resolve build failure due to missing import for @CrossOrigin

--- src/main/java/com/ps/covidportal/services/CovidSummaryRestController.java
+++ src/main/java/com/ps/covidportal/services/CovidSummaryRestController.java
@@ -3,10 +3,7 @@ package com.ps.covidportal.services;
 import com.ps.covidportal.services.resultbeans.CovidTotalSummary;
 import com.ps.covidportal.services.serviceHelpers.CovidSummaryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
